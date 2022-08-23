e01f1adc0029748aa4cf859b9ceab2eb72f630ef
Sagar Raj Thapaliya <68162024+sagarthapaliya12@users.noreply.github.com>
2022-08-23 17:05:50 +0545

    Added screens on bottom tab

---
+++ App/screens/Notifications.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+
+function Notifications(props) {
+return (
+   <View style={styles.container}>
+        <Text> this is notifications page</Text>
+   </View>
+);
+}
+
+const styles = StyleSheet.create({
+container: {}
+})
+
+export default Notifications;
