ee7f34516c518e7ff4e70b73fbb7a53848e879cb
Sagar Raj Thapaliya <68162024+sagarthapaliya12@users.noreply.github.com>
2022-08-30 23:57:35 +0545

    New Component for Listings in notifications

---
+++ App/components/ListItem.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	TouchableHighlight,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from "../config/colors";
+import Icon from "./Icon";
+
+const ListItem = ({ image, title, description, onPress, IconComponent }) => (
+	<TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+		<View style={styles.container}>
+			{IconComponent && <Icon />}
+			{image && <Image source={image} style={styles.image} />}
+			<View style={styles.details}>
+				<Text numberOfLines={1} style={styles.title}>
+					{title}
+				</Text>
+				{description && (
+					<Text numberOfLines={2} style={styles.description}>
+						{description}
+					</Text>
+				)}
+			</View>
+			<MaterialCommunityIcons
+				name="chevron-right"
+				size={24}
+				color={colors.medium}
+			/>
+		</View>
+	</TouchableHighlight>
+);
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: "row",
+		backgroundColor: colors.screen,
+		padding: 15,
+		alignItems: "center",
+	},
+	description: {
+		fontWeight: "300",
+		color: colors.medium,
+	},
+	details: {
+		justifyContent: "center",
+		marginLeft: 10,
+		flex: 1,
+	},
+	image: {
+		height: 50,
+		width: 50,
+		borderRadius: 50,
+	},
+	title: {
+        color: colors.white,
+		fontSize: 16,
+		fontWeight: "600",
+	},
+});
+
+export default ListItem;
