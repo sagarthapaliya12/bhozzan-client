f99c4b8457d1744aa98b74df168ae5bac0ed9e94
Sajag Pradhanang <spd.sajag@gmail.com>
2022-10-09 20:49:53 +0545

    separate navigation index

--- App.js
+++ App.js
@@ -1,27 +1,11 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import navigationTheme from "./App/navigation/navigationTheme";
 import { Provider as PaperProvider } from "react-native-paper";
-
-import CustomerTab from "./App/components/Customer/CustomerTab";
-import RestaurantTab from "./App/components/Restaurant/RestaurantTab";
-import PublicNavigator from "./App/navigation/PublicNavigator";
-import Dashboard from "./App/screens/Admin/Dashboard";
-
-const auth = {
-  user: { id: "6019e133h3e3sj72837283", name: "Jack Blah Blah Blah", role: "manager" },
-};
+import AppRoutes from "./App/navigation";
 
 export default function App() {
   return (
     <PaperProvider>
-      <NavigationContainer theme={navigationTheme}>
-        {!auth.user && <PublicNavigator />}
-        {auth.user?.role === "customer" && <CustomerTab />}
-        {auth.user?.role === "manager" && <RestaurantTab />}
-        {auth.user?.role === "admin" && <Dashboard />}
-        {/* {auth.user?.role === "shipper" && <Dashboard />} */}
-      </NavigationContainer>
+      <AppRoutes />
     </PaperProvider>
   );
 }
---
+++ App/navigation/index.js
@@ -0,0 +1,24 @@
+import { NavigationContainer } from "@react-navigation/native";
+import navigationTheme from "./navigationTheme";
+import CustomerTab from "../components/Customer/CustomerTab";
+import RestaurantTab from "../components/Restaurant/RestaurantTab";
+import PublicNavigator from "../navigation/PublicNavigator";
+import Dashboard from "../screens/Admin/Dashboard";
+
+const auth = {
+  user: { id: "6019e133h3e3sj72837283", name: "Jack Blah Blah Blah", role: "manager" },
+};
+
+const AppRoutes = () => {
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      {!auth.user && <PublicNavigator />}
+      {auth.user?.role === "customer" && <CustomerTab />}
+      {auth.user?.role === "manager" && <RestaurantTab />}
+      {auth.user?.role === "admin" && <Dashboard />}
+      {auth.user?.role === "shipper" && <Dashboard />}
+    </NavigationContainer>
+  );
+};
+
+export default AppRoutes;
