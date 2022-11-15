891e31934082babf1d800cdd70dcdf78b077aebe
Sajag <spd.sajag@gmail.com>
2022-11-15 21:34:43 +0545

    fix snack bar in category screen

--- App/screens/Customer/BrowseCategory.jsx
+++ App/screens/Customer/BrowseCategory.jsx
@@ -17,6 +17,8 @@ import { getDishesByCategory } from "../Restaurant/restaurantSlice";
 import { Ionicons } from "@expo/vector-icons";
 import { addToBasket } from "./customerSlice";
 import { toggleShowSnackbar } from "../../redux/ui/uiSlice";
+import Screen from "../../components/Screen";
+import SnackbarMessage from "../../components/SnackbarMessage";
 
 const { height, width } = Dimensions.get("window");
 
@@ -34,87 +36,62 @@ const BrowseCategory = () => {
     dispatch(toggleShowSnackbar(true));
   };
 
-  // const [noOfItem, setNoOfItem] = useState(0);
-
-  // const incrementCount = () => {
-  //   setNoOfItem((prevCount) => prevCount + 1);
-  //   // setNoOfItem({ ...noOfItem, [id]: ++noOfItem[id] });
-  // };
-
-  // const decrementCount = () => {
-  //   // console.log(noOfItem);
-  //   // noOfItem[id] >= 1 ? setNoOfItem({ ...noOfItem, [id]: --noOfItem[id] }) : '';
-  //   noOfItem >= 1 ? setNoOfItem((prevCount) => prevCount - 1) : "";
-  // };
   return (
-    <ScrollView style={styles.container}>
-      {/* <Text>{categoryState}</Text> */}
-      {dishes.map((food) => {
-        return (
-          <View key={food._id} style={styles.categoryItem}>
-            <View style={styles.itemDetail}>
-              <Text style={{ color: colors.gray, fontSize: 18 }}>{food.name}</Text>
-              <Text style={styles.restaurantName}>{food.restaurant.name}</Text>
-              <View style={styles.locationContainer}>
-                <Entypo
-                  name="location-pin"
-                  size={24}
-                  color={colors.primary}
-                  style={{ fontSize: 20 }}
-                />
-                <Text style={styles.location}>{food.restaurant.address}</Text>
-              </View>
-            </View>
-            <View style={styles.priceCart}>
-              <View>
-                <Text style={{ color: colors.secondary, fontSize: 20 }}>Rs.&nbsp;{food.price}</Text>
-              </View>
-              <TouchableHighlight onPress={() => handleBasket(food._id)}>
-                <View
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
-                    backgroundColor: colors.secondary,
-                    borderRadius: 15,
-                    marginTop: 5,
-                    paddingVertical: 5,
-                    paddingHorizontal: 10,
-                  }}
-                >
-                  <Ionicons name="basket" size={30} color={colors.screen} />
-                  <Text style={{ color: colors.v }}>Add To Basket</Text>
+    <Screen>
+      <ScrollView style={styles.container}>
+        {dishes.map((food) => {
+          return (
+            <View key={food._id} style={styles.categoryItem}>
+              <View style={styles.itemDetail}>
+                <Text style={{ color: colors.gray, fontSize: 18 }}>{food.name}</Text>
+                <Text style={styles.restaurantName}>{food.restaurant.name}</Text>
+                <View style={styles.locationContainer}>
+                  <Entypo
+                    name="location-pin"
+                    size={24}
+                    color={colors.primary}
+                    style={{ fontSize: 20 }}
+                  />
+                  <Text style={styles.location}>{food.restaurant.address}</Text>
                 </View>
-              </TouchableHighlight>
-              {/* <View style={styles.cart}>
-                <AntDesign
-                  name="minuscircle"
-                  size={24}
-                  color={colors.gray}
-                  onPress={() => decrementCount()}
-                />
-                <Text style={{ color: colors.gray, margin: 10, fontSize: 20 }}>{noOfItem}</Text>
-                <AntDesign
-                  name="pluscircle"
-                  size={24}
-                  color={colors.gray}
-                  onPress={() => incrementCount()}
-                />
-              </View> */}
+              </View>
+              <View style={styles.priceCart}>
+                <View>
+                  <Text style={{ color: colors.secondary, fontSize: 20 }}>
+                    Rs.&nbsp;{food.price}
+                  </Text>
+                </View>
+                <TouchableHighlight onPress={() => handleBasket(food._id)}>
+                  <View
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                      backgroundColor: colors.secondary,
+                      borderRadius: 15,
+                      marginTop: 5,
+                      paddingVertical: 5,
+                      paddingHorizontal: 10,
+                    }}
+                  >
+                    <Ionicons name="basket" size={30} color={colors.screen} />
+                    <Text style={{ color: colors.v }}>Add To Basket</Text>
+                  </View>
+                </TouchableHighlight>
+              </View>
             </View>
-          </View>
-        );
-      })}
-    </ScrollView>
+          );
+        })}
+      </ScrollView>
+      <SnackbarMessage subject="customer" />
+    </Screen>
   );
 };
 
 export default BrowseCategory;
 
 const styles = StyleSheet.create({
-  container: { backgroundColor: colors.screen },
   menuContainer: {
-    // backgroundColor: colors.gray,
     alignItems: "center",
   },
   title: {
