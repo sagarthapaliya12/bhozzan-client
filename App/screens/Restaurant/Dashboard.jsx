adfc10a71ce23fe42306b8dd2a93664275f71872
Saurav Ghimire <ghimiresaurav09@gmail.com>
2022-12-01 14:59:35 +0545

    implement notification on restaurant side

--- App/screens/Restaurant/Dashboard.jsx
+++ App/screens/Restaurant/Dashboard.jsx
@@ -1,14 +1,24 @@
 import React, { useEffect } from "react";
 import Screen from "../../components/Screen";
 import Paper from "../../components/Restaurant/Paper";
-import { useDispatch } from "react-redux";
-import {  getRestaurantUserId } from "./restaurantSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { getRestaurantUserId } from "./restaurantSlice";
+import socket from "../../utils/socket";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
 
+  const restaurantId = useSelector((state) => state.restaurantSlice.restaurantUserId);
+
   useEffect(() => {
     dispatch(getRestaurantUserId());
+    socket.emit("join", restaurantId);
+  }, [restaurantId]);
+
+  useEffect(() => {
+    socket.on("notification", (notification) => {
+      console.log("new notification", notification);
+    });
   }, []);
 
   return (
