15a73e6a810f2eb37d1453eea6628066df1b8eb6
Saurav Ghimire <ghimiresaurav09@gmail.com>
2022-12-01 14:33:42 +0545

    implement notification on customer side

--- App/screens/HomeScreen.jsx
+++ App/screens/HomeScreen.jsx
@@ -6,16 +6,22 @@ import socket from "../utils/socket";
 import React, { useEffect } from "react";
 import TopRestaurants from "../components/Customer/HomeScreen/TopRestaurants";
 import LocalCusines from "../components/Customer/HomeScreen/LocalCusines";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAllDishes } from "./Restaurant/restaurantSlice";
 import SearchBar from "./../components/Customer/HomeScreen/SearchBar";
 import TopDishes from "../components/Customer/HomeScreen/TopDishes";
 
 const HomeScreen = ({ navigation }) => {
+  const userId = useSelector((state) => state.authSlice.user.id);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAllDishes());
+    socket.emit("join", userId);
+
+    socket.on("notification", (notification) => {
+      console.log("new notification", notification);
+    });
   }, []);
 
   return (
